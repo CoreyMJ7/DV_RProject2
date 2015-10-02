b9f4e33fcc812f080d343b4a139f6131d0fe9c01
Corey Monreal-Jackson <corey.monreal.jackson@utexas.edu>
2015-10-02 16:05:08 -0500

    xsxsxsx

--- 02 Data Wrangling/Fortune500 Wrangling.R
+++ 02 Data Wrangling/Fortune500 Wrangling.R
@@ -1,6 +1,10 @@
 tbl_df(df)
 View(df)
 
-df %>% select(YEAR, RANK) %>% filter(YEAR %in% c("1955", "2004")) %>% View
-x <- df %>% group_by(RANK) %>% select(COMPANY, YEAR) %>% filter(RANK < 6) %>% arrange(YEAR) %>% View
-y <- df %>% select(COMPANY, REVENUE_IN_MILLIONS_, PROFIT_IN_MILLIONS_, YEAR, RANK) %>% filter(YEAR %in% c("1955", "2004")) %>% arrange(RANK) %>% View
+#v <- df %>% select(COMPANY, YEAR, PROFIT_IN_MILLIONS_, REVENUE_IN_MILLIONS_)  %>% filter(YEAR == 1955) %>% mutate(rev_percent = cume_dist(as.numeric(REVENUE_IN_MILLIONS_))) %>% tbl_df
+#x <- df %>% group_by(RANK, YEAR) %>% select(COMPANY, YEAR, PROFIT_IN_MILLIONS_) %>% filter(RANK < 6, YEAR<2005) %>% tbl_df
+#y <- df %>% select(COMPANY, REVENUE_IN_MILLIONS_, PROFIT_IN_MILLIONS_, YEAR, RANK) %>% filter(YEAR %in% c("1955", "2004")) %>% arrange(RANK) %>% tbl_df
+#View(v)
+#View(x)
+#View(y)
+
