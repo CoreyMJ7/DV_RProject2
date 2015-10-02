b7e0d00464899a8d082018dfa6ffba0684665d23
Nathan Riojas <nriojas.60@gmail.com>
2015-10-02 18:23:22 -0500

    no message

--- 02 Data Wrangling/Fortune500 Wrangling.R
+++ 02 Data Wrangling/Fortune500 Wrangling.R
@@ -1,10 +1,11 @@
-tbl_df(df)
-View(df)
 
-V <- df %>% select(COMPANY, YEAR, PROFIT_IN_MILLIONS_, REVENUE_IN_MILLIONS_)  %>% filter(YEAR == 1955) %>% mutate(rev_percent = cume_dist(as.numeric(REVENUE_IN_MILLIONS_))) %>% tbl_df
+
+V <- df %>% select(COMPANY, YEAR, PROFIT_IN_MILLIONS_, REVENUE_IN_MILLIONS_)  %>% filter(YEAR == 1955) %>% mutate(rev_percent = cume_dist(as.numeric(REVENUE_IN_MILLIONS_)), Ninety_Percentile= rev_percent > .90) %>% filter(rev_percent > .5) %>% tbl_df
 X <- df %>% group_by(RANK, YEAR) %>% select(COMPANY, YEAR, PROFIT_IN_MILLIONS_) %>% filter(RANK < 6, YEAR<2005) %>% tbl_df
 Y <- df %>% select(COMPANY, REVENUE_IN_MILLIONS_, PROFIT_IN_MILLIONS_, YEAR, RANK) %>% filter(YEAR %in% c("1955", "2004")) %>% arrange(RANK) %>% tbl_df
-#View(v)
-#View(x)
-#View(y)
+
+
+View(V)
+View(X)
+View(Y)
 
