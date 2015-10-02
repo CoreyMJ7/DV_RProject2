0cfd787085d5b4253370255496041f3aec21eb4e
Corey Monreal-Jackson <corey.monreal.jackson@utexas.edu>
2015-10-02 12:46:26 -0500

    z

--- 02 Data Wrangling/Fortune500 Wrangling.R
+++ 02 Data Wrangling/Fortune500 Wrangling.R
@@ -2,3 +2,5 @@ tbl_df(df)
 View(df)
 
 df %>% select(YEAR, RANK) %>% filter(YEAR %in% c("1955", "2004")) %>% View
+x <- df %>% group_by(RANK) %>% select(COMPANY, YEAR) %>% filter(RANK < 6) %>% arrange(YEAR) %>% View
+y <- df %>% select(COMPANY, REVENUE_IN_MILLIONS_, PROFIT_IN_MILLIONS_, YEAR, RANK) %>% filter(YEAR %in% c("1955", "2004")) %>% arrange(RANK) %>% View
