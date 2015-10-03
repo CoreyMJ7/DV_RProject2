0c12964caeca8df079a93f45825a394811256204
Nathan Riojas <nriojas.60@gmail.com>
2015-10-02 20:05:59 -0500

    Badass

--- 03 Visualizations/Companies Visualization.R
+++ 03 Visualizations/Companies Visualization.R
@@ -1,19 +1,17 @@
-#Our unique graph is found at the bottom of this code with a description of it in the comments.
+#Our unique graphs are found at the bottom of this code with a description of it in the comments.
 
 require(extrafont)
 
-#Graph 1: Fare vs Age w/ null included
+#Graph 1: This graph looks at the Fortune 500 companies in 1955 that had a revenue greater than $600 Million and highlights whether or not they are in the 90th percentile of the cumulative distribution function for profits.
+
 ggplot() + 
   coord_cartesian() + 
   scale_x_discrete() +
   scale_y_continuous() +
-  #facet_wrap(~SURVIVED) +
-  #facet_grid(.~SURVIVED, labeller=label_both) + # Same as facet_wrap but with a label.
-  #facet_grid(PCLASS~SURVIVED, labeller=label_both) +
-  labs(title='Titanic') +
+  labs(title='Revenue vs Company in 1955') +
   theme(axis.text.x = element_text(angle = 90, hjust = 1)) +
-  labs(x="Company", y=paste("Profit")) +
-  layer(data=V, 
+  labs(x="Company", y=paste("Revenue > 600 Million")) +
+  layer(data=Solo_1995, 
         mapping=aes((x=COMPANY), y=(Rev), color=Ninety_Percentile), 
         stat="identity", 
         stat_params=list(), 
@@ -22,4 +20,23 @@ ggplot() +
         #position=position_identity()
         position=position_jitter(width=0.3, height=0)
   )
+
+ggplot() + 
+  coord_cartesian() + 
+  scale_x_discrete() +
+  scale_y_continuous() +
+  labs(title='Titanic') +
+  theme(axis.text.x = element_text(angle = 90, hjust = 1)) +
+  labs(x="SURVIVED", y=paste("FARE")) +
+  layer(data=Y, 
+        mapping=aes(x=as.character(YEAR), y=as.numeric(as.character(RANK)), color=COMPANY), 
+        stat="identity", 
+        stat_params=list(), 
+        geom="point",
+        geom_params=list(), 
+        #position=position_identity()
+        position=position_jitter(width=0.5, height=0)
+  )
+
+
 
